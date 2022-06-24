5dd95e5635a9f0ab5cea4e774360701f9d2368c7
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-24 17:43:53 +0530

    added emp pay roll service message on master branch

---
+++ src/main/java/com/bridge/emp/EmpPayRollData.java
@@ -0,0 +1,22 @@
+package com.bridge.emp;
+
+public class EmpPayRollData {
+   public int id;
+   public String name;
+    public double salary;
+
+    public EmpPayRollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpPayRollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/main/java/com/bridge/emp/EmpPayRollService.java
@@ -0,0 +1,11 @@
+package com.bridge.emp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmpPayRollService {
+    public static void main(String[] args) {
+        System.out.println("employee pay roll service");
+    }
+}
